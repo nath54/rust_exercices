e4836631faf33b60136e347515d0f057c61d428c
nath54 <=>
2020-07-25 14:23:54 +0200

    aa

--- dif_btw_squares_additions/src/main.rs
+++ dif_btw_squares_additions/src/main.rs
@@ -1,14 +1,26 @@
 
-fn calc_sum_square(N: u32) -> u32{
+fn calc_sum_square(n: u32) -> u32{
     let mut result = 0;
-    
+    for x in 0..n{
+        result+=x*x;
+    }   
+    return result;
+}
+
+fn calc_sum_simple(n: u32) -> u32{
+    let mut result = 0;
+    for x in 0..n{
+        result+=x;
+    }   
     return result;
 }
 
 
 
-
-
 fn main() {
-    println!("Hello, world!");
+    let n=0;
+    let sum_square=calc_sum_square(n);
+    let sum_simple=calc_sum_simple(n);
+    let final_result=sum_square-sum_simple;
+    println!("The result of the sum of the squares of all integers between 0 and {} and theirs addition is {}", n, final_result);
 }
